486d58ceb742a5598c571d969f19c35736646249
odion <murphy.usunobun@dci-student.org>
2026-01-15 11:26:39 +0100

    fitst

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    const getMessage = async () => {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/message`)
+      const data = await res.json()
+      console.log(data)
+      setMessage(data.message)
+    }
+
+    getMessage()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Frontend Render Example</h1>
+      <p>Here is a connection to our server</p>
+
+      {message.length < 0 ? <p>Loading...</p> : <p>Message: {message}</p>}
     </>
   )
 }
